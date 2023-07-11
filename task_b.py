f294d3e36a6b53a5e028c8d88584d5cca55d93a3
Manav054 <74268080+Manav054@users.noreply.github.com>
2023-07-11 13:48:24 +0530

    task_b.py

--- task_b.py
+++ task_b.py
@@ -22,22 +22,18 @@ async def calculate_similarity(text1, text2) :
   value = cosine_similarity(sent1, sent2)[0]
   return round(value[0], 4)
 
-def main() :
-  st.title("Semantic Similarity Tester")
-  html_temp = """
-  <div style = "backgroud-color : tomato ; padding : 10px">
-  <h2 style = "color : white ; text-align : center ;>Streamlit Semantic Textual Similarity App</h2>
-  </div>
-  """
-  st.markdown(html_temp, unsafe_allow_html = True)
-  text1 = st.text_input("Text1", "Type Here")
-  text2 = st.text_input("Text2", "Type Here")
-  similarity = ""
+st.title("Semantic Similarity Tester")
+html_temp = """
+<div style = "backgroud-color : tomato ; padding : 10px">
+<h2 style = "color : white ; text-align : center ;>Streamlit Semantic Textual Similarity App</h2>
+</div>
+"""
+st.markdown(html_temp, unsafe_allow_html = True)
+text1 = st.text_input("Text1", "Type Here")
+text2 = st.text_input("Text2", "Type Here")
+similarity = ""
 
-  if st.button("Calculate similarity") :
+if st.button("Calculate similarity") :
     similarity = await asyncio.run(calculate_similarity)(text1, text2)
-  st.success("Similarity is {}".format(similarity))
-
-if __name__ == '__main__' :
-  main()
+st.success("Similarity is {}".format(similarity))
 
