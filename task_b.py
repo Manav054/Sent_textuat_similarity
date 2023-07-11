2660558eb6636e5a8e3a7da1295882084ea97c04
Manav054 <74268080+Manav054@users.noreply.github.com>
2023-07-11 13:46:25 +0530

    task_b.py

--- task_b.py
+++ task_b.py
@@ -16,13 +16,13 @@ import asyncio
 
 model = SentenceTransformer("bert-base-nli-mean-tokens")
 
-def calculate_similarity(text1, text2) :
+async def calculate_similarity(text1, text2) :
   sent1 = model.encode(text1).reshape(1, -1)
   sent2 = model.encode(text2).reshape(1, -1)
   value = cosine_similarity(sent1, sent2)[0]
   return round(value[0], 4)
 
-async def main() :
+def main() :
   st.title("Semantic Similarity Tester")
   html_temp = """
   <div style = "backgroud-color : tomato ; padding : 10px">
